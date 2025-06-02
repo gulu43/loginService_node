403d7bcb7d7d98a88deb236c896003cec7850095
gulu43 <gulamnabi9th@gmail.com>
2025-06-02 10:55:39 +0530

    but changes in response on frontend side

--- utils/index.js
+++ utils/index.js
@@ -14,10 +14,10 @@ app.use(cors({
 }));
 
 // Logger Middleware
-app.use((req, res, next) => {
-  console.log(`[${req.method}] ${req.url}`);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(`[${req.method}] ${req.url}`);
+//   next();
+// });
 
 app.use(express.json())
 app.use("/user", router)
